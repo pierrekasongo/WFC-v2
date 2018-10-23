4b06ec989cfc1e9588fa9cf2fb389b962d09a6b5
pierrekasongo <pierrekasongo88@gmail.com>
2018-10-23 19:17:04 +0100

    Adding home page
    Adding home page

---
+++ app/user/HomePage.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Collapsible from 'react-collapsible';
+import { Panel, Form, FormGroup, ControlLabel, FormControl, Col, Checkbox,PanelGroup,Accordion } from 'react-bootstrap';
+
+export default class HomePage extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div className="intro-screen">
+                <Panel bsStyle="primary" header="Home">
+                    
+                </Panel>
+                
+            </div>
+        );
+    }
+
+};
