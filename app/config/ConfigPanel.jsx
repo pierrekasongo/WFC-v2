ce6735783f07b15d165240a738be24e31a471aa6
pierrekasongo <pierrekasongo88@gmail.com>
2019-01-11 12:00:36 +0100

    Adding inline editing
    Adding inline editing for parameter's value

--- app/config/ConfigPanel.jsx
+++ app/config/ConfigPanel.jsx
@@ -3,21 +3,43 @@ import * as React from 'react';
 import { Panel, Form, FormGroup, ControlLabel, Button, FormControl, Col, Checkbox, Table } from 'react-bootstrap';
 import * as axios from 'axios';
 
+import InlineEdit from 'react-edit-inline2';
+
 export default class ConfigPanel extends React.Component {
 
     constructor(props) {
         super(props);
-       
+
+        this.handleChange = this.handleChange.bind(this);
+
         this.state = {
             configs: {},
-            filter: ""
+            filter: "",
         }
-        
+
         axios.get('/configuration/configs')
             .then(res => this.setState({ configs: res.data }))
             .catch(err => console.log(err));
     }
+    validateValue(text) {
+        return (text.length > 0 && text.length < 64);
+    }
 
+    handleChange(obj) {
+
+        const id = Object.keys(obj)[0];
+
+        const value=Object.values(obj)[0];
+
+        let data = {
+            value: value,
+        };
+        axios.patch('/configuration/config/' + id, data).then(res => {
+
+            console.log('Value updated successfully');
+
+        }).catch(err => console.log(err));
+    }
 
     getFilteredConfigs() {
         return Object.keys(this.state.configs).filter(val => {
@@ -50,22 +72,27 @@ export default class ConfigPanel extends React.Component {
                         {this.getFilteredConfigs().map(paramId =>
                             <tr key={paramId}>
                                 <td>{this.state.configs[paramId].parameter}</td>
-                                <td>{this.state.configs[paramId].value}</td>
-                                {/*<td><InlineEdit activeClassName="editing"
-                                    text={this.state.configs[paramId].value}
-                                    paramName="message"
-                                    change={this.valueChanged}
-                                    style={{
-                                        backgroundColor: 'yellow',
-                                        minWidth: 150,
-                                        display: 'inline-block',
-                                        margin: 0,
-                                        padding: 0,
-                                        fontSize: 15,
-                                        outline: 0,
-                                        border: 0
-                                    }}/>
-                                </td>*/}
+                                <td>
+                                    <div>
+                                        <InlineEdit
+                                            validate={this.validateValue}
+                                            activeClassName="editing"
+                                            text={this.state.configs[paramId].value}
+                                            paramName={this.state.configs[paramId].id}
+                                            change={this.handleChange}
+                                            style={{
+                                                /*backgroundColor: 'yellow',*/
+                                                minWidth: 150,
+                                                display: 'inline-block',
+                                                margin: 0,
+                                                padding: 0,
+                                                fontSize: 15,
+                                                outline: 0,
+                                                border: 0
+                                            }}
+                                        />
+                                    </div>
+                                </td>
                             </tr>
                         )}
                     </tbody>
@@ -73,5 +100,4 @@ export default class ConfigPanel extends React.Component {
             </div>
         );
     }
-
 }
