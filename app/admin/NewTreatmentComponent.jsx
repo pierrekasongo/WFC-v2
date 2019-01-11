7840936b1949badee947afce104544eb4a00d6bf
pierrekasongo <pierrekasongo88@gmail.com>
2019-01-11 11:55:23 +0100

    Adding Bulk adding
    Adding the bulk adding component to the page

--- app/admin/NewTreatmentComponent.jsx
+++ app/admin/NewTreatmentComponent.jsx
@@ -12,7 +12,7 @@ export default class NewTreatmentComponent extends React.Component {
             cadreId: Object.keys(props.cadres)[0],
             duration:0
         }
-        //console.log(Object.keys(this.props.cadres));
+        //console.log(props.treatments);
     }
     render() {
         return (
@@ -22,11 +22,11 @@ export default class NewTreatmentComponent extends React.Component {
                             componentClass="select"
                             onChange={e => this.setState({ treatmentId: e.target.value })}
                             value={this.state.treatmentId}>
-                            {Object.keys(this.props.treatments).map(treatmentId =>
+                            {Object.keys(this.props.treatments).map(treatment =>
                                 <option
-                                    key={treatmentId.Id}
-                                    value={treatmentId.Id}>
-                                    {this.props.treatments[treatmentId]}
+                                    key={this.props.treatments[treatment].id}
+                                    value={this.props.treatments[treatment].id}>
+                                    {this.props.treatments[treatment].activityName}
                                 </option>
                             )}
                     </FormControl>
@@ -38,8 +38,8 @@ export default class NewTreatmentComponent extends React.Component {
                         value={this.state.cadreId}>
                         {Object.keys(this.props.cadres).map(cadre =>
                             <option
-                                key={cadre.id}
-                                value={cadre.id}>
+                                key={this.props.cadres[cadre].id}
+                                value={this.props.cadres[cadre].id}>
                                 {this.props.cadres[cadre].name}
                             </option>
                         )}
@@ -49,12 +49,11 @@ export default class NewTreatmentComponent extends React.Component {
                     <FormControl
                         type="number"
                         value={this.state.duration}
-                        onChange={e => this.setState({duration:e.target.value})}
-                    />
+                        onChange={e => this.setState({duration:e.target.value})} />
                 </td>
                 <td>
-                    <Button onClick={() => this.props.save(this.state)}>Save</Button>
-                    <Button onClick={() => this.props.cancel()}>Cancel</Button>
+                    <Button bsStyle="warning" className="btn-bulk" bsSize="small" onClick={() => this.props.cancel()}>Cancel</Button>
+                    <Button bsStyle="warning" className="btn-bulk" bsSize="small" onClick={() => this.props.save(this.state)}>Save</Button>
                 </td>
             </tr>
         );
