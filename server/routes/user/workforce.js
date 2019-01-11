9b2bef2291c9c45027f64abea4dbb19119df4145
pierrekasongo <pierrekasongo88@gmail.com>
2019-01-11 12:18:16 +0100

    Adding multiple facilities calculation
    Handling multiple facilities pressure calculation

--- server/routes/user/workforce.js
+++ server/routes/user/workforce.js
@@ -2,44 +2,38 @@ const sql = require('mysql');
 
 let router = require('express').Router();
 
-const db=require('../../dbconn');
+const db = require('../../dbconn');
 
-router.post('/', (req, res) => {
+router.post('/:code', (req, res) => {
 
-    /* what req.body looks like:
-        { facilityId: '8',
-  cadres:
-   { '1': { hours: 40, adminPercentage: 17 },
-     '2': { hours: 43, adminPercentage: 15 } },
-  treatments:
-   { '1': true,
-     '2': false,
-     '3': true,
-     '15': true,
-     '16': true,
-     '17': true,
-     '18': true } }
-    */
-    let cadres=req.body.cadres;
-    let treatmentIds =['1','2','3','4','5','6','7','8']; //Object.keys(req.body.treatments).filter(id => req.body.treatments[id]);
-    let facilityId=req.body.facilityId.toString();
-    let period=req.body.selectedPeriod.toString();
-    console.log(period);
-    
-    // queries
-    let treatmentsQuery = `SELECT Id, Ratio FROM treatments`;
-    let patientCountQuery = `SELECT TreatmentId, SUM(Patients) AS PatientCount FROM dhis2
-                                WHERE CONCAT(Year, Quarter)=ANY(SELECT DISTINCT CONCAT(Year, Quarter) FROM dhis2 WHERE FacilityId="`+facilityId+`" ORDER BY CONCAT(Year, Quarter) DESC)
-                                    AND FacilityId="`+facilityId+`"
-                                GROUP BY TreatmentId LIMIT 0,4`;
-    let timePerTreatmentQuery = `SELECT TreatmentId, CadreId, MinutesPerPatient AS TreatmentTime FROM TreatmentSteps  GROUP BY TreatmentId, CadreId`;//Select only for selected cadres
-    let facilityStaffCountQuery = `SELECT CadreId, COUNT(*) AS StaffCount FROM ihris
-                                    WHERE  FacilityCode="`+facilityId+`"
-                                    GROUP BY CadreId`;
+    let cadres = req.body.cadres;
 
-    //new sql.Request()
-       // .input('FacilityId', sql.Int, req.body.facilityId)
-        db.query(`${treatmentsQuery}; ${patientCountQuery}; ${timePerTreatmentQuery}; ${facilityStaffCountQuery};`,function(error,results){
+    let period = req.body.selectedPeriod.toString();
+
+    let facilityId = req.params.code.toString();
+
+    //Getting cadre ids 
+    let cadreIds = [];
+    let i = 0;
+    Object.keys(cadres).forEach(cadreId => {
+        cadreIds[i] = cadreId;
+        i++;
+    });
+                           
+        let treatmentIds = [];
+        // queries
+        //let concernedTreatmentsQuery= `SELECT id, ratio FROM activities WHERE `;
+        let treatmentsQuery = `SELECT id, ratio FROM activities`;
+        //let treatmentsQuery = `SELECT id, ratio FROM activities WHERE id IN (SELECT activityId FROM activity_time WHERE cadreId IN(?) )`;
+        let patientCountQuery = `SELECT activityId  AS id, SUM(caseCount) AS PatientCount FROM activity_stats
+                                WHERE CONCAT(year, quarter)=ANY(SELECT DISTINCT CONCAT(year, quarter) FROM activity_stats WHERE facilityId="`+ facilityId + `" AND year="`+ period + `" ORDER BY CONCAT(year, quarter) DESC)
+                                    AND facilityId="`+ facilityId + `" GROUP BY activityId LIMIT 0,4`;
+        let timePerTreatmentQuery = `SELECT activityId, cadreId, minutesPerPatient AS TreatmentTime FROM activity_time WHERE cadreId IN(?) GROUP BY activityId, cadreId`;//Select only for selected cadres
+        let facilityStaffCountQuery = `SELECT cadreId, staffCount AS StaffCount FROM staff
+                                    WHERE  facilityCode="`+ facilityId + `"`;
+                                    
+        db.query(`${treatmentsQuery}; ${patientCountQuery}; ${timePerTreatmentQuery}; ${facilityStaffCountQuery};`, [cadreIds, cadreIds], 
+        function (error, results) {
 
             let treatmentsQueryResult = results[0];
 
@@ -48,52 +42,61 @@ router.post('/', (req, res) => {
             let facilityStaffCountQueryResult = results[3];
 
             // convert results from query into a dictionary from an array
-            let patientsPerTreatment = {}
+            let patientsPerTreatment = {};
+
             patientCountQueryResult.forEach(row => {
-                patientsPerTreatment[row['TreatmentId']] = row['PatientCount'];
+                patientsPerTreatment[row['id']] = row['PatientCount'];
+
+                treatmentIds[row['id']] = row['id'];
             });
 
             let timePerTreatmentQueryResult = results[2];
 
-            //console.log(timePerTreatmentQueryResult);
-
             // set to zero if treatment has no patients
             treatmentsQueryResult.forEach(row => {
-                if (!patientsPerTreatment[row['Id']]) {
-                    patientsPerTreatment[row['Id']] = 0
+                if (!patientsPerTreatment[row['id']]) {
+                    patientsPerTreatment[row['id']] = 0
                 }
             });
 
             let workersNeededPerTreatment = {};
+
             Object.keys(cadres).forEach(cadreId => {
+
                 treatmentsQueryResult.forEach(treatmentRow => {
-                    let treatmentId = treatmentRow['Id'];
+
+                    let treatmentId = treatmentRow['id'];
 
                     let timePerPatient = timePerTreatmentQueryResult.filter(val =>
-                        val['CadreId'] == cadreId && val['TreatmentId'] == treatmentId);
+                        val['cadreId'] == cadreId && val['activityId'] == treatmentId);
+
                     if (timePerPatient[0] == null) {
                         timePerPatient = 0;
                     } else {
                         timePerPatient = timePerPatient[0]['TreatmentTime'];
                     }
 
-                    totalHoursForTreatment = (timePerPatient / 60) * patientsPerTreatment[treatmentId];
+                    //timePerPatient = timePerPatient[0]['TreatmentTime'];
 
+                    totalHoursForTreatment = (timePerPatient / 60) * patientsPerTreatment[treatmentId];
 
                     // input parameters
                     let cadreHours = cadres[cadreId].hours;
+
                     let cadreAdminPercentage = cadres[cadreId].adminPercentage;
 
                     let hoursAWeek = cadreHours * (1 - (cadreAdminPercentage / 100));
+
                     let hoursAYear = hoursAWeek * 52;
 
                     if (workersNeededPerTreatment[cadreId] == null) {
+
                         workersNeededPerTreatment[cadreId] = {};
                     }
 
                     workersNeededPerTreatment[cadreId][treatmentId] = totalHoursForTreatment / hoursAYear;
+
                 });
-                
             });
 
             // sum workers needed for only selected treatments
@@ -104,66 +107,69 @@ router.post('/', (req, res) => {
                     workersNeeded[cadreId] += workersNeededPerTreatment[cadreId][treatmentId]
                 )
             });
-            
 
             /******* calculate workforce pressure ***************/
             // step 1: normalize ratio values
             let ratioSum = 0;
-            treatmentsQueryResult.forEach(row => ratioSum += row['Ratio']);
+            treatmentsQueryResult.forEach(row => ratioSum += row['ratio']);
             let normalizedRatios = {};
             treatmentsQueryResult.forEach(row =>
-                normalizedRatios[row['Id']] = row['Ratio'] / ratioSum
+                normalizedRatios[row['id']] = row['ratio'] / ratioSum
             );
             // step 2: determine current workforce dedicated to each treatment
             let workersPerTreatment = {};
+
             facilityStaffCountQueryResult.forEach(row => {
-                workersPerTreatment[row['CadreId']] = {};
+
+                workersPerTreatment[row['cadreId']] = {};
+
                 Object.keys(normalizedRatios).forEach(treatmentId => {
-                    workersPerTreatment[row['CadreId']][treatmentId] = row['StaffCount'] * normalizedRatios[treatmentId];
+
+                    workersPerTreatment[row['cadreId']][treatmentId] = row['StaffCount'] * normalizedRatios[treatmentId];
                 });
             });
-            
+
             // step 3: calculate pressure, but only for the selected treatments
             let pressure = {}
             let currentWorkers = {};
             Object.keys(workersPerTreatment).forEach(cadreId => {
+
                 let workers = 0;
-                
+
                 treatmentIds.forEach(treatmentId => {
-                    
+
                     workers += workersPerTreatment[cadreId][treatmentId];
-                    
+
                 });
                 //workers=2;
                 //currentWorkers[cadreId] = 2;
                 currentWorkers[cadreId] = Number.parseFloat(workers).toFixed(2);
-                
-                pressure[cadreId] =Number.parseFloat(workers).toFixed(2) / Number.parseFloat(workersNeeded[cadreId]).toFixed(2);
+
+                pressure[cadreId] = Number.parseFloat(workers).toFixed(2) / Number.parseFloat(workersNeeded[cadreId]).toFixed(2);
             });
 
             //Current workers: suggested by Pierre; simply select the available workers for the cadre
             facilityStaffCountQueryResult.forEach(row => {
 
-                currentWorkers[row['CadreId']] = row['StaffCount'];
+                currentWorkers[row['cadreId']] = row['StaffCount'];
 
-                pressure[row['CadreId']] =Number.parseInt(row['StaffCount']) / Number.parseFloat(workersNeeded[row['CadreId']]).toFixed(2);
+                pressure[row['cadreId']] = Number.parseInt(row['StaffCount']) / Number.parseFloat(workersNeeded[row['cadreId']]).toFixed(2);
             });
+
+            /*calculationResults[id]={
+                facility:facilities[id].name,
+                currentWorkers:currentWorkers,
+                workersNeeded: workersNeeded,
+                pressure: pressure
+            }*/
             
             res.json({
                 currentWorkers: currentWorkers,
                 workersNeeded: workersNeeded,
                 pressure: pressure
             });
-
-            /*console.log(currentWorkers);
-            console.log(workersNeeded);
-            console.log(pressure);*/
+            
         });
-        /*db.then(result => {*/
-            
-            
-        //}).catch(err => res.status(500).json(err));
-});
-
+})
 
 module.exports = router;
